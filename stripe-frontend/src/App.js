eaf8b2d9faa145cd5b1bb3be7f1ac2da1a09cc59
Rishabh Tripathi <la-rishabh@mobiloitte.com>
2022-12-20 07:01:12 +0530

    stripe integration in react v18, node v16, express 4.17, stripe v9

---
+++ stripe-frontend/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+import CheckoutForm from "./Checkout";
+import "./App.css";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe("pk_test_51JaDndSIyZlIaC71hWySgs0x57n6EGTWFfXyCvgemPNzJuOaQI9siqD0KdFYIIkYEgLaD4T77wMxV3d5JLSzCBiG003H9pQwU7");
+
+export default function App() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:4242/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
